eb007cf7ad0dceba4bc98649c7ea6f12c4290190
anarkia1976 <stefano.villa1976@gmail.com>
2013-11-26 10:42:43 +0100

    drivers: input: 2w: enable s2w and dt2w debug

--- drivers/input/touchscreen/sweep2wake.c
+++ drivers/input/touchscreen/sweep2wake.c
@@ -52,7 +52,7 @@ MODULE_VERSION(DRIVER_VERSION);
 MODULE_LICENSE("GPLv2");
 
 /* Tuneables */
-#define S2W_DEBUG		0
+#define S2W_DEBUG		1
 #define S2W_DEFAULT		0
 #define S2W_PWRKEY_DUR          60
 
