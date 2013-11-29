c5ecc960b1ba2f153e45b6efcf24e7b4f486c0e2
anarkia1976 <stefano.villa1976@gmail.com>
2013-11-29 11:22:35 +0100

    drivers: input: 2w: rework s2s and now you can use dt2w with this feature

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -2169,7 +2169,7 @@ static void touch_early_suspend(struct early_suspend *h)
         bool prevent_sleep = false;
 #endif
 #if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
-        prevent_sleep = (s2w_switch > 0) && (s2w_s2sonly == 0);
+        prevent_sleep = (s2w_switch == 1);
 #endif
 #if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
         prevent_sleep = prevent_sleep || (dt2w_switch > 0);
@@ -2222,7 +2222,7 @@ static void touch_late_resume(struct early_suspend *h)
         bool prevent_sleep = false;
 #endif
 #if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
-        prevent_sleep = (s2w_switch > 0) && (s2w_s2sonly == 0);
+        prevent_sleep = (s2w_switch == 1);
 #endif
 #if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
         prevent_sleep = prevent_sleep || (dt2w_switch > 0);
