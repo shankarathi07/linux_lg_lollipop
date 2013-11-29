66ec5e5ba1ab5b9e7129edfca9591ce9e51c1bde
Francisco Franco <franciscofranco.1990@gmail.com>
2013-11-29 12:22:22 -0500

    KGSL: Simple Governor: so I kinda butchered in a good way of course the original code from faux, but the simplicity is still here. Added an history so that we can track the average do that the frequency scaling is much more proportional to the actual GPU load rather than having is ramp up every now and the due to spurious load spike. Also remove two useless ifs. Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -9,6 +9,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ * Modified by Paul Reioux (Faux123)
+ * 2013-06-20: Added KGSL Simple GPU Governor
+ *
  */
 
 #include <linux/export.h>
@@ -146,50 +149,51 @@ static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 
 
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-/* KGSL Simple GPU Governor */
-/* Copyright (c) 2011-2013, Paul Reioux (Faux123). All rights reserved. */
-static int default_laziness = 5;
-module_param_named(simple_laziness, default_laziness, int, 0664);
+#define HISTORY_SIZE 10
 
-static int ramp_up_threshold = 6000;
+static int ramp_up_threshold = 5500;
 module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
-static int laziness;
+static unsigned int history[HISTORY_SIZE] = {0};
+static unsigned int counter = 0;
 
 static int simple_governor(struct kgsl_device *device, int idle_stat)
 {
-    int val = 0;
     struct kgsl_pwrctrl *pwr = &device->pwrctrl;
+    int i;
+    unsigned int total = 0;
+    
+    history[counter] = idle_stat;
+    
+    for (i = 0; i < HISTORY_SIZE; i++)
+        total += history[i];
+    
+    total = total/HISTORY_SIZE;
+    
+    if (++counter == 10)
+        counter = 0;
     
     /* it's currently busy */
-    if (idle_stat < ramp_up_threshold) {
-        if (pwr->active_pwrlevel == 0)
-            val = 0; /* already maxed, so do nothing */
-        else if ((pwr->active_pwrlevel > 0) &&
-                 (pwr->active_pwrlevel <= (pwr->num_pwrlevels - 1)))
-            val = -1; /* bump up to next pwrlevel */
-        /* idle case */
-    } else {
+    if (total < ramp_up_threshold)
+    {
+        if ((pwr->active_pwrlevel > 0) &&
+            (pwr->active_pwrlevel <= (pwr->num_pwrlevels - 1)))
+        /* bump up to next pwrlevel */
+            return -1;
+    }
+    /* idle case */
+    else
+    {
         if ((pwr->active_pwrlevel >= 0) &&
             (pwr->active_pwrlevel < (pwr->num_pwrlevels - 1)))
-            if (laziness > 0) {
-                /* hold off for a while */
-                laziness--;
-                val = 0; /* don't change anything yet */
-            } else {
-                val = 1; /* above min, lower it */
-                /* reset laziness count */
-                laziness = default_laziness;
-            }
-            else if (pwr->active_pwrlevel == (pwr->num_pwrlevels - 1))
-                val = 0; /* already @ min, so do nothing */
+            return 1;
     }
-    return val;
+    
+    return 0;
 }
 #endif
 
-static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale,
-                    unsigned int ignore_idle)
+static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
     struct kgsl_pwrctrl *pwr = &device->pwrctrl;
     struct tz_priv *priv = pwrscale->priv;
@@ -274,7 +278,7 @@ static int tz_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
     if (pwrscale->priv == NULL)
         return -ENOMEM;
     
-    priv->governor = TZ_GOVERNOR_ONDEMAND;
+    priv->governor = TZ_GOVERNOR_SIMPLE;
     spin_lock_init(&tz_lock);
     kgsl_pwrscale_policy_add_files(device, pwrscale, &tz_attr_group);
     
