4f037818d29efb1d1408198281dd9b018dde8624
anarkia1976 <stefano.villa1976@gmail.com>
2013-12-02 22:18:58 +0100

    drivers: input: 2w: cleaned s2w_s2sonly unused external integer

--- include/linux/input/sweep2wake.h
+++ include/linux/input/sweep2wake.h
@@ -21,7 +21,7 @@
 #ifndef _LINUX_SWEEP2WAKE_H
 #define _LINUX_SWEEP2WAKE_H
 
-extern int s2w_switch, s2w_s2sonly;
+extern int s2w_switch;
 extern bool s2w_error;
 extern struct input_dev * doubletap2wake_pwrdev;
 
